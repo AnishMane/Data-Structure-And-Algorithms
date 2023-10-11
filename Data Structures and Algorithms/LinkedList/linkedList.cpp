eed2c73a8e61143d4f4f8021e55718f3afbfe6c1
AnishMane <anishmane70@gmail.com>
2023-10-11 12:35:02 +0530

    linkedList

---
+++ Data Structures and Algorithms/LinkedList/linkedList.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* createNode(int data)
+{
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+}
+
+int main()
+{
+
+}
